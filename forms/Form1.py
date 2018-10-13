752ec10470fb2c2bb05175a2913c58142e483149
hugetim@gmail.com <hugetim@gmail.com>
2018-10-13 22:56:42 +0000

    Saved automatically

--- forms/Form1.py
+++ forms/Form1.py
@@ -1,11 +1,29 @@
 from anvil import *
+import anvil.tables as tables
+from anvil.tables import app_tables
+import anvil.users
 
 class Form1(Form1Template):
-
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+    while not anvil.users.login_with_form():
+      pass
+
+  def request_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if app_tables.offers.get() == None:
+      new_row = app_tables.requests.add_row(name=anvil.users.get_user(), start_time=datetime.datetime.now())
+      self.status.text = "Status: Requesting empathy. Awaiting an offer..."
+    else:
+      oldest_offer = app_tables.offers.search(tables.order_by("start_time", ascending=True))[:1]
+      make_match(anvil.users.get_user(),oldest_offer[0]['name'])
+      
+def make_match(requester, offerer):
+  self.status.text = "Making match with ", requester, " and ", offerer
+
+
 
 
