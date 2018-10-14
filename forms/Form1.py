810f2d365df28200604d69ac2e78d0ee8f8075b8
hugetim@gmail.com <hugetim@gmail.com>
2018-10-14 23:33:19 +0000

    Saved automatically

--- forms/Form1.py
+++ forms/Form1.py
@@ -15,9 +15,24 @@ class Form1(Form1Template):
 
   def request_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call('add_request',anvil.users.get_user().get_id())
-    self.status.text = "Status: Requesting empathy. Awaiting an offer..."
-      
+    jitsi_code = anvil.server.call('add_request',anvil.users.get_user().get_id())
+    if jitsi_code == None:
+      self.status.text = "Status: Requesting empathy. Awaiting an offer..."
+    else:
+      self.status.text = "Use Jitsi " + jitsi_code
+    self.request_button.visible = False
+    self.offer_button.visible = False
+
+  def offer_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    jitsi_code = anvil.server.call('add_offer',anvil.users.get_user().get_id())
+    if jitsi_code == None:
+      self.status.text = "Status: Offering empathy. Awaiting a request..."
+    else:
+      self.status.text = "Use Jitsi " + jitsi_code
+    self.request_button.visible = False
+    self.offer_button.visible = False
+    
 
   def timer_1_tick(self, **event_args):
     """This method is called Every [interval] seconds"""
--- server_modules/MatchMaker.py
+++ server_modules/MatchMaker.py
@@ -2,6 +2,8 @@ import anvil.tables as tables
 from anvil.tables import app_tables
 import anvil.users
 import anvil.server
+import random
+import datetime
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -19,9 +21,49 @@ import anvil.server
 @anvil.server.callable
 def add_request(user_id):
     if app_tables.matches.get() == None:
-      new_row = app_tables.matches.add_row(request_id=anvil.users.get_user().get_id())
+      add_request_row(user_id)
     else:
-      #make match or add a new request row
-      pass
-      #top_row = list(app_tables.offers.search(tables.order_by("start_time", ascending=True))[:1])[0]
-      #self.make_match(anvil.users.get_user().get_id(),oldest_offer[0]['name'])
+      offers = [row for row in app_tables.matches.search()
+            if row["offer_id"] != None and row["request_id"] == None]
+      # if no offers, add new row, else add request info
+      if not offers:
+        add_request_row(user_id)
+      else:
+        offers.sort(key = lambda row: row['offer_time'])
+        earliest_offer = offers[0]
+        earliest_offer['request_id'] = user_id
+        earliest_offer['request_time'] = datetime.datetime.now()
+        return create_jitsi(earliest_offer)
+
+@anvil.server.callable
+def add_offer(user_id):
+    if app_tables.matches.get() == None:
+      add_offer_row(user_id)
+    else:
+      requests = [row for row in app_tables.matches.search()
+            if row["request_id"] != None and row["offer_id"] == None]
+      # if no offers, add new row, else add request info
+      if not requests:
+        add_offer_row(user_id)
+      else:
+        requests.sort(key = lambda row: row['request_time'])
+        earliest_request = requests[0]
+        earliest_request['offer_id'] = user_id
+        earliest_request['offer_time'] = datetime.datetime.now()
+        return create_jitsi(earliest_request)
+
+def add_request_row(user_id):
+  new_row = app_tables.matches.add_row(request_id=anvil.users.get_user().get_id(), request_time=datetime.datetime.now())
+  return new_row
+
+def add_offer_row(user_id):
+  new_row = app_tables.matches.add_row(offer_id=anvil.users.get_user().get_id(), offer_time=datetime.datetime.now())
+  return new_row
+
+def create_jitsi(match):
+  random.seed()
+  randint = random.randint(1,1000000000)
+  code = "empathy" + str(randint)
+  match['jitsi_code'] = code
+  return code
+  
