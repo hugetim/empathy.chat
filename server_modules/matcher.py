74dbc916ec59e38c7c305bacb8ebc8555bee2acb
hugetim@gmail.com <hugetim@gmail.com>
2019-02-26 02:45:37 +0000

    fix: Initialize new user's request/match_em (and prev. commit altered unauth. user behavior)

--- server_modules/matcher.py
+++ server_modules/matcher.py
@@ -473,9 +473,9 @@ def _get_user_info(user_id=""):
   user = _get_user(user_id)
   trust = user['trust_level']
   if trust is None:
-    user.update(trust_level=0)
-    assert user['request_em'] == False
-    assert user['match_em'] == False
+    user['trust_level'] == 0
+    user['request_em'] == False
+    user['match_em'] == False
   return user['trust_level'], user['request_em'], user['match_em']
 
 
