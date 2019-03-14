59379d87aa2ef2edc94c0998bfc4475fc128c2f0
hugetim@gmail.com <hugetim@gmail.com>
2019-03-14 01:31:46 +0000

    feat: Add title bar to LoginForm

--- forms/LoginForm.py
+++ forms/LoginForm.py
@@ -1,16 +1,18 @@
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.google.auth, anvil.google.drive
-import anvil.google.auth
-import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.users
-
 
 class LoginForm(LoginFormTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
     
   def form_show (self, **event_args):
     # Do the code here to show this blank form.
