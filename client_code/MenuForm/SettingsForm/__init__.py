ec037d8660403e2c09cc62bc35cc39e2d800d3cc
hugetim@gmail.com <hugetim@gmail.com>
2020-09-03 23:13:30 +0000

    feat: continue creating SettingsForm

--- client_code/MenuForm/SettingsForm/__init__.py
+++ client_code/MenuForm/SettingsForm/__init__.py
@@ -1,21 +1,14 @@
 from ._anvil_designer import SettingsFormTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
+
 
 class SettingsForm(SettingsFormTemplate):
-  def __init__(self, **properties):
+  def __init__(self, re, re_opts, re_st, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
     self.init_request_em_opts(re, re_opts, re_st)
-
-    # Any code you write here will run when the form opens.
     
   def request_em_check_box_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""
