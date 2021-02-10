59ce6dfff6890e82942bb0cec56d9b63ecf88ed0
hugetim@gmail.com <hugetim@gmail.com>
2021-02-10 19:10:38 +0000

    switch to prod parameters

--- client_code/parameters.py
+++ client_code/parameters.py
@@ -1,9 +1,9 @@
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 BUFFER_SECONDS = 7
-CONFIRM_MATCH_SECONDS = 15 #60
-WAIT_SECONDS = 55 #60*20
-ASSUME_INACTIVE_DAYS = 60 #30 day persistent login + 30 days
+CONFIRM_MATCH_SECONDS = 60 #15
+WAIT_SECONDS = 60*20 #55
+ASSUME_INACTIVE_DAYS = 60 # 30 day persistent login + 30 days
 MIN_BETWEEN_R_EM = 20
 CONTACT_EMAIL = "empathyspot@gmail.com"
 URL_WITH_ALT = "https://e6hfysk4hhri7btc.anvil.app/FEJAOY5Q5ZBKEJWWCKZNNCJZ" #"https://www.empathyspot.org (or https://minty-sarcastic-telephone.anvil.app )"
