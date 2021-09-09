0c42d90e338c41e315e2b76c4c47f66d075fcfff
hugetim@gmail.com <hugetim@gmail.com>
2021-09-09 16:02:10 +0000

    Created MenuForm.MyGroupsForm.MyGroupsRow; Edited MenuForm.MyGroupsForm and MenuForm.ConnectionsMenu.Connections.ConnectionRow

---
+++ client_code/MenuForm/MyGroupsForm/MyGroupsRow.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import MyGroupsRowTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+class MyGroupsRow(MyGroupsRowTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
