7f7e6e80fc8ee82f325ca267b3c481bf5e8d2032
hugetim@gmail.com <hugetim@gmail.com>
2021-09-09 17:12:36 +0000

    Arrange client code (minor)

--- client_code/MenuForm/GroupsMenu/Groups/GroupTemplate/__init__.py
+++ client_code/MenuForm/GroupsMenu/Groups/GroupTemplate/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import GroupTemplateTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
 
-class RowTemplate1(RowTemplate1Template):
+class GroupTemplate(GroupTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
