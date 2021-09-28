e1a009985f24359d7e2b29d15353379cec67e97c
hugetim@gmail.com <hugetim@gmail.com>
2021-09-28 22:12:22 +0000

    Create InviteA form

---
+++ client_code/MenuForm/NetworkMenu/InviteA/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import InviteATemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+class InviteA(InviteATemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+
+  def relationship_text_box_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    self.phone_last4_text_box.focus()
+
