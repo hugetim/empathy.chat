5f1dc2234425cf06b6f219a49e03e1b9697a53be
hugetim@gmail.com <hugetim@gmail.com>
2022-01-05 23:54:18 +0000

    Fix no-name users in Invites table

--- client_code/MenuForm/NetworkMenu/Invites/InviteRow/__init__.py
+++ client_code/MenuForm/NetworkMenu/Invites/InviteRow/__init__.py
@@ -17,11 +17,14 @@ class InviteRow(InviteRowTemplate):
     # Any code you write here will run when the form opens.
     user2 = self.item.get('invitee')
     if user2:
-      self.link.visible = False
-      user2_name = user2.name if user2.name else "[as-yet nameless user]"
-      name_item = {'name': user2_name, 'confirmed_url': user2.confirmed_url, 'user_id': user2.user_id}
-      self.name = Name(item=name_item)
-      self.name_or_url_flow_panel.add_component(self.name)
+      if user2.name:
+        self.link.visible = False
+        name_item = {'name': user2.name, 'confirmed_url': user2.confirmed_url, 'user_id': user2.user_id}
+        self.name = Name(item=name_item)
+        self.name_or_url_flow_panel.add_component(self.name)
+      else:
+        self.link.visible = False
+        self.name_or_url_flow_panel.add_component(Label(text="[user registered, name pending]"))
     else:
       self.link.url = self.item.url
       self.link.text = "Invite Link"
