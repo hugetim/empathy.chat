46b1f5c210a07a7e46cfa9e894d332e75b1272c5
hugetim@gmail.com <hugetim@gmail.com>
2022-02-22 23:51:23 +0000

    Simplify Connections Degree/Group column heading if only one or other

--- client_code/MenuForm/NetworkMenu/Connections/__init__.py
+++ client_code/MenuForm/NetworkMenu/Connections/__init__.py
@@ -22,4 +22,12 @@ class Connections(ConnectionsTemplate):
     items = anvil.server.call('get_connections', self.item['user_id'])
     items.sort(key=lambda pu: (pu.distance_str_or_groups, h.reverse_compare(pu['last_active'])))
     self.repeating_panel_1.items = items
+    has_degrees = any([item.distance_str for item in items])
+    has_groups = any([not item.distance_str and item.common_group_names for item in items])
+    if not has_degrees or not has_groups:
+      columns = list(self.data_grid_1.columns)
+      [degree_column] = [column for column in columns if column['data_key'] == "degree"]
+      degree_index = columns.index(degree_column)
+      columns[degree_index]['title'] = "Degree" if has_degrees else "Group(s)"
+      self.data_grid_1.columns = columns
     self.refresh_data_bindings()
