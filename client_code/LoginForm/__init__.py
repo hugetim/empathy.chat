067734fa301f09981dc28d00da185368897e9946
hugetim@gmail.com <hugetim@gmail.com>
2022-03-17 15:56:08 +0000

    Overhaul LoginForm

--- client_code/LoginForm/__init__.py
+++ client_code/LoginForm/__init__.py
@@ -13,26 +13,32 @@ class LoginForm(LoginFormTemplate):
     
   def form_show(self, **event_args):
     # Do the code here to show this blank form.
-    while not anvil.users.get_user():
-      user = anvil.users.login_with_form(show_signup_option=False)
+    self.login_sequence()
+    
+  def login_sequence(self):
+    if not anvil.users.get_user():
+      user = anvil.users.login_with_form(show_signup_option=False, allow_cancel=True)
       if user and (not user['init_date']):
         anvil.users.logout()
         with Notification("Creating a new account requires an invite link."):
           anvil.server.call_s('remove_user', user)
           import time
-          time.sleep(2)  
-    self.card_1.visible = True
-    from .. import ui_procedures as ui
-    self.init_dict = ui.get_init()
-    ui.get_mobile_status()
-    self.new_form = ui.reload(self.init_dict, do_open=False)
-    from .. import parameters
-    if parameters.DEBUG_MODE:
-      self.enter_button_click()
-    else:
-      from anvil_extras import augment
-      self.enter_button.visible = True
-      self.enter_button.trigger('focus')
+          time.sleep(2)
+    if anvil.users.get_user():
+      self.login_button.visible = False
+      self.rich_text_1.visible = False
+      self.card_1.visible = True
+      from .. import ui_procedures as ui
+      self.init_dict = ui.get_init()
+      ui.get_mobile_status()
+      self.new_form = ui.reload(self.init_dict, do_open=False)
+      from .. import parameters
+      if parameters.DEBUG_MODE:
+        self.enter_button_click()
+      else:
+        from anvil_extras import augment
+        self.enter_button.visible = True
+        self.enter_button.trigger('focus')
 
   def enter_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -50,3 +56,7 @@ class LoginForm(LoginFormTemplate):
     from .. import ui_procedures as ui
     from .. import parameters
     self.timer_1.interval = 0
+
+  def login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.login_sequence()
