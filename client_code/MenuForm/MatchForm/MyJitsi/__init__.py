5e525283b781980ad0c37305224c8f2a0fc79bc5
hugetim@gmail.com <hugetim@gmail.com>
2022-08-05 18:24:13 +0000

    Show name in Jitsi video

--- client_code/MenuForm/MatchForm/MyJitsi/__init__.py
+++ client_code/MenuForm/MatchForm/MyJitsi/__init__.py
@@ -22,7 +22,7 @@ class MyJitsi(MyJitsiTemplate):
 #         domain = "8x8.vc"
 #         room_name = f"vpaas-magic-cookie-848c456481fc4755aeb61d02b9d9dab2/{self.item['room_name']}"
       self.call_js("initJitsi", self.item['domain'],
-                   {"roomName": room_name, "height": "100%", "width": "100%", "userInfo": {"displayName": " "}},
+                   {"roomName": room_name, "height": "100%", "width": "100%", "userInfo": {"displayName": self.item['name']}},
                    self.item['room_name']
                   ) #self.item['name']
       
