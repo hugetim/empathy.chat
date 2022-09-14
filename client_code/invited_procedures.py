b2d19cb4a3cd0436d12b6beb522d4aec47410a33
hugetim@gmail.com <hugetim@gmail.com>
2022-09-14 18:23:47 +0000

    Edited glob and invited_procedures

--- client_code/invited_procedures.py
+++ client_code/invited_procedures.py
@@ -7,6 +7,7 @@ from . import ui_procedures as ui
 from . import helper as h
 from . import groups
 from .glob import publisher
+from . import glob
 from .exceptions import RowMissingError, ExpiredInviteError, MistakenVisitError, InvalidInviteError, MistakenGuessError
 from .Dialogs.Invited import Invited
 
