8c9ab5cb49cf3c8be1970c832df5d66eca5a1dc7
hugetim@gmail.com <hugetim@gmail.com>
2022-11-02 15:06:08 +0000

    Clean up auto_test

--- client_code/auto_test.py
+++ client_code/auto_test.py
@@ -1,37 +1,8 @@
 from anvil import *
 import anvil.server
-import unittest
 import time
 
-    
-# class MyGroupsTest(unittest.TestCase):
-#   def setUp(self):
-#     pass
-#     #self.my_groups = groups.MyGroups()
-#     #self.my_groups = anvil.server.call('load_my_groups)
-    
-#   def test_get_items(self):
-#     items = self.my_groups.drop_down_items()
-    
-#   def test_add_group(self):
-#     new_group = self.my_groups.add_group()
-#     self.assertEqual(new_group['name'], "New Group")
 
-
-# def client_auto_tests():
-#   from anvil_extras.utils import timed
-  
-#   @timed
-#   def tests_run_client_side():
-#     from . import exchange_controller_test as ect
-#     ect.ExchangeControllerTest().main()
-#   tests_run_client_side()
-  
-  
-def test_alert(content, handler):
-  content.set_event_handler('x-close-alert', handler)
-  
-  
 class TestNow(): #not unittest.TestCase
   def setUp(self):
     self.top_form = get_open_form()
@@ -58,25 +29,7 @@ class TestNow(): #not unittest.TestCase
 
     
 def run_now_test():
-  #unittest.main()
-  #print("run manually")
   test = TestNow()
   test.setUp()
   test.test_repeat_now_proposal()
   #test.test_multiple_proposal_times()
-
-# class TestNow(unittest.TestCase):
-
-#   def setUp(self):
-#     self.top_form = get_open_form()
-#     self.top_form.go_dash()
-
-#   def test_add_my_now_proposal(self):
-#     self.top_form.content.propose_button_click()
-#     focused_component = get_focused_component()
-#     print(focused_component.__dict__())
-#     #self.assertEqual( multiply(3,4), 12)
-
-
-#def run_tests():
-#   unittest.main()
