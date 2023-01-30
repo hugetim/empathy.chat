c2d6b32b76fad27a0f9785245e95618ae52dc2bd
hugetim@gmail.com <hugetim@gmail.com>
2023-01-30 19:30:13 +0000

    Edited requests

--- client_code/requests.py
+++ client_code/requests.py
@@ -12,7 +12,7 @@ class Eformat(Datum):
   #   self.duration = duration
 
 
-@anvil.server.portable_class 
+@anvil.server.portable_class
 class Request():
   # id: str
   # or_group_id: str
@@ -49,11 +49,25 @@ class Request():
     self.eligible_starred = eligible_starred
     self.current = current
 
+  def __repr__(self):
+    return (
+      f"Request({self.request_id!r}, {self.or_group_id!r}, {self.user!r}, {self.start_dt!r}, {self.eformat!r}, {self.expire_dt!r}, "
+      f"{self.create_dt!r}, {self.edit_dt!r}, {self.min_size!r}, {self.max_size!r}, "
+      f"{self.eligible!r}, {self.eligible_users!r}, {self.eligible_groups!r}, {self.eligible_starred!r}, "
+      f"{self.current!r})"
+    )
+  
+  def __str__(self):
+    return (
+      f"Request({self.request_id!s}, {self.or_group_id!s}, {self.user!s}, {self.start_dt!s}, {self.eformat!s}, {self.expire_dt!s}, "
+      f"{self.create_dt!s}, {self.edit_dt!s}, {self.min_size!s}, {self.max_size!s}, "
+      f"{self.eligible!s}, {self.eligible_users!s}, {self.eligible_groups!s}, {self.eligible_starred!s}, "
+      f"{self.current!s})"
+    )
+  
   @property
-  def start_now(self, now=None):
-    if now is None:
-      now = datetime.now()
-    return self.start_dt < now and self.expire_dt > now
+  def start_now(self):
+    return self.start_dt < self.expire_dt
 
   @property
   def end_dt(self):
@@ -66,16 +80,74 @@ class Request():
         return True
     return False
 
-  def get_potential_matches(self, other_user_requests):
+  def get_prospects(self, other_exchange_prospects):
     out = []
-    for other in other_user_requests:
-      if (self.start_dt == other.start_dt 
-          and self.eformat == other.eformat
+    for other in other_exchange_prospects:
+      if (self.eformat == other.eformat
           and self.min_size <= other.max_size
           and self.max_size >= other.min_size
+          and not other.is_full
+          and (self.start_dt == other.start_dt or (self.start_now and other.start_now))
          ):
-        out.append({self, other})
+        out.append(other.plus_request(self))
     return out
+
+
+@anvil.server.portable_class 
+class ExchangeProspect:
+  def __init__(self, requests):
+    if not requests or len(requests) < 1 or not isinstance(requests[0], Request):
+      raise ValueError("Input 'requests' must contain at least one Request")
+    self._requests = tuple(requests)
+
+  def __repr__(self):
+    return f"ExchangeProspect({', '.join(repr(r) for r in self._requests)})"
+
+  def __str__(self):
+    return f"{{{', '.join(str(r) for r in self._requests)}}}"
+  
+  def plus_request(self, request):
+    if self.is_full:
+      raise RuntimeError("Cannot add request because ExchangeProspect is full")
+    return ExchangeProspect(self._requests + (request,))
+  
+  def __len__(self):
+    return len(self._requests)
+
+  def __getitem__(self, index):
+    return self._requests[index]
+
+  @property
+  def min_size(self):
+    return max([r.min_size for r in self._requests])
+
+  @property
+  def max_size(self):
+    return min([r.max_size for r in self._requests])
+
+  @property
+  def _rep_request(self):
+    return self._requests[0]
+  
+  @property
+  def eformat(self):
+    return self._rep_request.eformat
+
+  @property
+  def start_dt(self):
+    return self._rep_request.start_dt
+
+  @property
+  def start_now(self):
+    return self._rep_request.start_now
+
+  @property
+  def has_enough(self):
+    return len(self) >= self.min_size
+
+  @property
+  def is_full(self):
+    return len(self) >= self.max_size
 
 
 def have_conflicts(requests):
@@ -91,7 +163,29 @@ def have_conflicts(requests):
 
 
 def potential_matches(new_requests, other_user_requests):
-  out = []
+  exchange_prospects = []
+  other_exchange_prospects = all_exchange_prospects(other_user_requests)
   for new_request in new_requests:
-    out.extend(new_request.get_potential_matches(other_user_requests))
-  return out
+    exchange_prospects.extend(new_request.get_prospects(other_exchange_prospects))
+  return [set(ep) for ep in exchange_prospects]
+
+
+def all_exchange_prospects(requests):
+  return _exchange_prospects(requests)
+                            
+                          
+def _exchange_prospects(remaining_requests, prospects_so_far=None):
+  if prospects_so_far is None:
+    prospects_so_far = []
+  if not remaining_requests:
+    return prospects_so_far
+  this_proposer = remaining_requests[0].user
+  this_proposer_requests = [r for r in remaining_requests if r.user == this_proposer]
+  other_user_requests = [r for r in remaining_requests if r.user != this_proposer]
+  new_prospects = []
+  for r in this_proposer_requests:
+    new_prospects.extend(r.get_prospects(prospects_so_far) + [ExchangeProspect([r])])
+  prospects_so_far.extend(new_prospects)
+  return _exchange_prospects(other_user_requests, prospects_so_far)
+  
+  
