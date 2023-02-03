8f28c1a33ff4f5e9ec03297aef3c74038b67238f
hugetim@gmail.com <hugetim@gmail.com>
2023-02-03 02:34:48 +0000

    Clean up requests

--- client_code/requests.py
+++ client_code/requests.py
@@ -8,28 +8,15 @@ from datetime import datetime, timedelta
 @anvil.server.portable_class 
 class Eformat(Datum):
   duration: int
-  # def __init__(self, duration=None):
-  #   self.duration = duration
 
 
 @anvil.server.portable_class
-class Request():
-  # id: str
-  # or_group_id: str
-  # user: User
-  # start_dt: datetime
-  # eformat: Eformat
-  # expire_dt: datetime
-  # create_dt: datetime
-  # edit_dt: datetime
-  # min_size: int = 2
-  # max_size: int = 2
-  # eligible: int
-  # eligible_users: list
-  # eligible_groups: list
-  # eligible_starred: bool
-  def __init__(self, request_id=None, or_group_id=None, user=None, start_dt=None, eformat=None, expire_dt=None,
-               create_dt=None, edit_dt=None, min_size=2, max_size=2,
+class Request:
+  def __init__(self, request_id=None, or_group_id=None,
+               user=None,
+               start_dt=None, eformat=None, expire_dt=None,
+               create_dt=None, edit_dt=None,
+               min_size=2, max_size=2,
                eligible=None, eligible_users=None, eligible_groups=None, eligible_starred=None,
                current=None,
               ):
@@ -126,6 +113,14 @@ class ExchangeProspect:
     return min([r.max_size for r in self._requests])
 
   @property
+  def has_enough(self):
+    return len(self) >= self.min_size
+
+  @property
+  def is_full(self):
+    return len(self) >= self.max_size
+    
+  @property
   def _rep_request(self):
     return self._requests[0]
   
@@ -138,54 +133,51 @@ class ExchangeProspect:
     return self._rep_request.start_dt
 
   @property
+  def end_dt(self):
+    return self.start_dt + timedelta(minutes=self.eformat.duration)
+  
+  @property
   def start_now(self):
     return self._rep_request.start_now
-
-  @property
-  def has_enough(self):
-    return len(self) >= self.min_size
-
-  @property
-  def is_full(self):
-    return len(self) >= self.max_size
 
 
 def have_conflicts(requests):
   # keep in sync with portable.Proposal.has_conflict
-  or_groups = list({r.or_group_id for r in requests})
-  for i, or_group in enumerate(or_groups):
-    remaining_or_groups = or_groups[i+1:]
-    non_or_requests = [r for r in requests if r.or_group_id in remaining_or_groups]
-    for r in [r for r in requests if r.or_group_id == or_group]:
-      if r.has_conflict(non_or_requests):
+  or_groups = [[r for r in requests if r.or_group_id == or_group_id]
+               for or_group_id in {r.or_group_id for r in requests}]
+  for i, this_or_group in enumerate(or_groups):
+    remaining_non_or_requests = [r for or_group in or_groups[i+1:] for r in or_group]
+    for r in this_or_group:
+      if r.has_conflict(remaining_non_or_requests):
         return True
   return False
 
 
 def potential_matches(new_requests, other_user_requests):
-  exchange_prospects = []
   other_exchange_prospects = all_exchange_prospects(other_user_requests)
+  exchange_prospects = []
   for new_request in new_requests:
     exchange_prospects.extend(new_request.get_prospects(other_exchange_prospects))
   return [set(ep) for ep in exchange_prospects]
 
 
 def all_exchange_prospects(requests):
-  return _exchange_prospects(requests)
+  return _gather_exchange_prospects(requests)
                             
                           
-def _exchange_prospects(remaining_requests, prospects_so_far=None):
-  if prospects_so_far is None:
-    prospects_so_far = []
+def _gather_exchange_prospects(remaining_requests, prospects_so_far=None):
+  if prospects_so_far is None: prospects_so_far = []
   if not remaining_requests:
     return prospects_so_far
   this_proposer = remaining_requests[0].user
   this_proposer_requests = [r for r in remaining_requests if r.user == this_proposer]
+  new_prospects = _combine_requests_w_prospects(this_proposer_requests, prospects_so_far)
   other_user_requests = [r for r in remaining_requests if r.user != this_proposer]
+  return _gather_exchange_prospects(other_user_requests, prospects_so_far + new_prospects)
+
+
+def _combine_requests_w_prospects(this_proposer_requests, prospects_so_far):
   new_prospects = []
   for r in this_proposer_requests:
     new_prospects.extend(r.get_prospects(prospects_so_far) + [ExchangeProspect([r])])
-  prospects_so_far.extend(new_prospects)
-  return _exchange_prospects(other_user_requests, prospects_so_far)
-  
-  
+  return new_prospects
