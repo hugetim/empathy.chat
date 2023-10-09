dd31194366b20abdd64a0f09f240085f9ce10d4b
hugetim@gmail.com <hugetim@gmail.com>
2023-10-09 02:59:07 +0000

    Put ei.commence_user_exchange in transaction

--- server_code/exchange_interactor.py
+++ server_code/exchange_interactor.py
@@ -59,6 +59,7 @@ def current_user_exchange(user, to_join=False, record=False):
   return out
 
 
+@tables.in_transaction
 def commence_user_exchange(user):
   exchange_record = current_user_exchange(user, to_join=True, record=True)
   exchange_record.commence()
