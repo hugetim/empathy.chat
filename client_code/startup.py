9f8ef034781af85a90bed6139bbad9094d941d52
hugetim@gmail.com <hugetim@gmail.com>
2023-10-30 14:43:35 +0000

    Fix Offline/Timeout error handling

--- client_code/startup.py
+++ client_code/startup.py
@@ -5,7 +5,7 @@ def error_handler(err):
   from . import error_helper as eh
   if isinstance(err, (anvil.server.AppOfflineError, anvil.server.TimeoutError)):
     eh.handle_server_connection_error(err)
-  if isinstance(err, anvil.users.AuthenticationFailed):
+  elif isinstance(err, anvil.users.AuthenticationFailed):
     eh.handle_authentication_error(err)
   else:
     eh.report_error(err)
