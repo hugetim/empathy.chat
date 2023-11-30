5684de9ffa530dac78f9868181d23848da1c2b35
hugetim@gmail.com <hugetim@gmail.com>
2023-11-30 00:28:15 +0000

    Edited MenuForm.DashForm.CreateForm and MenuForm.DashForm.CreateForm.TimeProposalTemplate

--- client_code/MenuForm/DashForm/CreateForm/TimeProposalTemplate/__init__.py
+++ client_code/MenuForm/DashForm/CreateForm/TimeProposalTemplate/__init__.py
@@ -14,67 +14,29 @@ class TimeProposalTemplate(TimeProposalTemplateTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.drop_down_duration.items = list(zip(t.DURATION_TEXT.values(), t.DURATION_TEXT.keys()))
-    self.drop_down_cancel.items = list(zip(t.CANCEL_TEXT.values(), t.CANCEL_TEXT.keys()))[1:]
-    self.normalize_initial_state()
     self.init_date_picker_start()
-    self.date_picker_cancel_initialized = False
     
   def form_show(self, **event_args):
     """This method is called when the column panel is shown on the screen"""
     self.alert_form = get_open_form().proposal_alert
     self.update()
-    
-  def normalize_initial_state(self):
-    if self.item['duration'] not in t.DURATION_TEXT.keys():
-      self.item['duration'] = t.closest_duration(self.item['duration'])
-    if self.item['cancel_buffer'] not in t.CANCEL_TEXT.keys():
-      self.item['cancel_date'] = (self.item['start_date'] 
-                                  - datetime.timedelta(minutes=self.item['cancel_buffer']))
-      self.item['cancel_buffer'] = "custom"
-  
+     
   def init_date_picker_start(self):
     defaults = t.ProposalTime.default_start()
     self.date_picker_start.min_date = defaults['s_min']
     self.date_picker_start.max_date = defaults['s_max']
     
-  def init_date_picker_cancel(self):
-    now=h.now()
-    self.date_picker_cancel.min_date = now
-    self.date_picker_cancel.max_date = self.date_picker_start.max_date
-    if not self.item['cancel_date']:
-      self.item['cancel_date'] = t.default_cancel_date(now, self.item['start_date'])
-    self.date_picker_cancel_initialized = True
-
   def update(self):
-    if self.item['cancel_buffer'] == "custom":
-      if not self.date_picker_cancel_initialized:
-        self.init_date_picker_cancel()
-      self.date_picker_cancel.visible = True
-    else:
-      self.date_picker_cancel.visible = False
     self.check_times()
     self.refresh_data_bindings()
-      
-  def drop_down_duration_change(self, **event_args):
-    """This method is called when an item is selected"""
-    self.update()
-    
-  def drop_down_cancel_change(self, **event_args):
-    """This method is called when an item is selected"""
-    self.update()
-    
+        
   def date_picker_start_change(self, **event_args):
     """This method is called when the selected date changes"""
     self.update()
-
-  def date_picker_cancel_change(self, **event_args):
-    """This method is called when the selected date changes"""
-    self.update()    
-    
+   
   def check_times(self):
     self.label_start.visible = False
-    self.label_cancel.visible = False
+    self.alert_form.label_cancel.visible = False
     messages = self.proptime().get_errors(self.alert_form.item['conflict_checks'])
     if messages:
       self.update_save_ready(False)
@@ -82,7 +44,7 @@ class TimeProposalTemplate(TimeProposalTemplateTemplate):
         self.label_start.text = messages['start_date']
         self.label_start.visible = True
       elif 'cancel_buffer' in messages:
-        self.label_cancel.text = messages['cancel_buffer']
+        self.alert_form.label_cancel.text = messages['cancel_buffer']
         self.label_cancel.visible = True
     else:
       self.update_save_ready(True)
